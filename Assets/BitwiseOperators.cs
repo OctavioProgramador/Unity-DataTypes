5cf9b447a2bec75f705e49adb59b2606249941db
OctavioProgramador <3amsro@gmail.com>
2023-05-01 20:59:52 -0700

    First commit

---
+++ Assets/BitwiseOperators.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BitwiseOperators : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        uint a,b,c;
+        a = 32;
+        b = a >> 3;
+        c = a << 3;
+        Debug.Log($"a,b,c = {a} {b} {c}");
+        uint d,e,f;
+        a = 0x04700_8999;
+        b = 0x0ffff_0000;
+        c = a & b;  //AND
+        d = a | b;  //OR
+        e = a ^ b;  //XOR
+        f = ~a;     //Complement
+        Debug.Log($"a = {a:x}");
+        Debug.Log($"b = {b:x}");
+        Debug.Log($"c = {c:x}");
+        Debug.Log($"d = {d:x}");
+        Debug.Log($"e = {e:x}");
+        Debug.Log($"f = {f:x}");
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
